5929dd196415e79f0b4e6e6c7b82ac9dfe232e89
Shashank <shashank.singh@caastle.com>
2024-03-09 14:46:46 +0530

    Port setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ console.log('Client side JS loaded')
 const fetchWeather = (location) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+    fetch('/weather?address='+ location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
